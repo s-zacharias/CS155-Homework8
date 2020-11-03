c6c66564ca5afe7963b94814a3eb808d2969b37f
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-11-02 17:16:00 -0800

    initial commit

---
+++ Project1/Duelist.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Project1
+{
+    public class Duelist
+    {
+        // instance variables
+        String name;
+        double shootingAccuracy;
+        bool alive;
+
+        // Properties
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double ShootingAccuracy
+        {
+            get
+            {
+                return shootingAccuracy;
+            }
+            set
+            {
+                shootingAccuracy = value;
+            }
+        }
+
+        public bool Alive
+        {
+            get
+            {
+                return alive;
+            }
+            set
+            {
+                alive = value;
+            }
+        }
+
+        // method
+        public void ShootAtTarget(Duelist target)
+        {
+            Random rnd = new Random();
+            
+            if (target.Alive == true)
+            {
+                double random = rnd.NextDouble();
+                if (random < (this.shootingAccuracy))
+                {
+                    target.Alive = false;
+                }
+            }
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create duelists
+            Duelist Aaron = new Duelist() { Name = "Aaron", ShootingAccuracy = (1.0 / 3), Alive = true };
+            Duelist Bob = new Duelist() { Name = "Bob", ShootingAccuracy = 0.5, Alive = true };
+            Duelist Charlie = new Duelist() { Name = "Charlie", ShootingAccuracy = 0.995, Alive = true };
+
+            // keep track of winners
+            int AaronWin = 0;
+            int BobWin = 0;
+            int CharlieWin = 0;
+
+            // number of duels
+            int duels = 10000;
+
+            for (int i = 0; i < duels; i++)
+            {
+                while ((Aaron.Alive && Bob.Alive) || (Aaron.Alive && Charlie.Alive)
+                || (Bob.Alive && Charlie.Alive))
+                {
+                    if (Aaron.Alive)
+                    {
+                        if (Charlie.Alive)
+                        {
+                            Aaron.ShootAtTarget(Charlie);
+                            if (Charlie.Alive)
+                            {
+                                Console.WriteLine("Aaron missed Charlie!");
+                            } else
+                            {
+                                Console.WriteLine("Aaron got Charlie!");
+                            }
+                        }
+                        else if (Bob.Alive)
+                        {
+                            Aaron.ShootAtTarget(Bob);
+                            if (Bob.Alive)
+                            {
+                                Console.WriteLine("Aaron missed Bob!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Aaron got Bob!");
+                            }
+                        }
+                    }
+                    if (Bob.Alive)
+                    {
+                        if (Charlie.Alive)
+                        {
+                            Bob.ShootAtTarget(Charlie);
+                            if (Charlie.Alive)
+                            {
+                                Console.WriteLine("Bob missed Charlie!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Bob got Charlie!");
+                            }
+                        }
+                        else if (Aaron.Alive)
+                        {
+                            Bob.ShootAtTarget(Aaron);
+                            if (Aaron.Alive)
+                            {
+                                Console.WriteLine("Bob missed Aaron!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Bob got Aaron!");
+                            }
+                        }
+                    }
+                    if (Charlie.Alive)
+                    {
+                        if (Bob.Alive)
+                        {
+                            Charlie.ShootAtTarget(Bob);
+                            if (Bob.Alive)
+                            {
+                                Console.WriteLine("Charlie missed Bob!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Charlie got Bob!");
+                            }
+                        }
+                        else if (Aaron.Alive)
+                        {
+                            Charlie.ShootAtTarget(Aaron);
+                            if (Aaron.Alive)
+                            {
+                                Console.WriteLine("Charlie missed Aaron!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Charlie got Aaron!");
+                            }
+                        }
+                    }
+                }
+                if (Aaron.Alive)
+                {
+                    AaronWin++;
+                } else if (Bob.Alive)
+                {
+                    BobWin++;
+                } else if (Charlie.Alive)
+                {
+                    CharlieWin++;
+                }
+            }
+            Console.WriteLine("Aaron won: " + AaronWin);
+            Console.WriteLine("Bob won: " + BobWin);
+            Console.WriteLine("Charlie won: " + CharlieWin);
+        }
+    }
+}
